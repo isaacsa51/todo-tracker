b19938460fc9528dba56d3904399d6159920b8a9
Isaac Serrano <serranoie99@gmail.com>
2021-07-11 14:51:30 -0600

    Changed reminder function to updated within the DB

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const App = () => {
 	const [showForm, setShowForm] = useState(false);
 	const [tasks, setTasks] = useState([]);
 
+	//TODO: CHANGE EVERY FUNCTION TO A MVP PROJECT
 	useEffect(() => {
 		const getTasks = async () => {
 			const tasksFromServer = await fetchTasks();
@@ -24,32 +25,54 @@ const App = () => {
 		return data;
 	};
 
-	//TODO: CHANGE EVERY FUNCTION TO A MVP PROJECT
+	const fetchTask = async (id) => {
+		const res = await fetch(`http://localhost:5000/tasks/${id}`);
+		const data = await res.json();
+
+		return data;
+	};
+
 	const addTask = async (task) => {
-		const res = await fetch(`http://localhost:5000/tasks`, {
+		const res = await fetch("http://localhost:5000/tasks", {
 			method: "POST",
 			headers: {
-				"Content-type": "application-json",
+				"Content-type": "application/json",
 			},
 			body: JSON.stringify(task),
 		});
 
 		const data = await res.json();
+
 		setTasks([...tasks, data]);
 	};
 
 	const deleteTask = async (id) => {
-		await fetch(`http://localhost:5000/tasks/${id}`, {
-			method: "DETELE",
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: "DELETE",
 		});
-
-		setTasks(tasks.filter((task) => task.id !== id));
+		//We should control the response status to decide if we will change the state or not.
+		res.status === 200
+			? setTasks(tasks.filter((task) => task.id !== id))
+			: alert("Error Deleting This Task");
 	};
 
-	const toggleReminder = (id) => {
+	const toggleReminder = async (id) => {
+		const taskToToggle = await fetchTask(id);
+		const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+		const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: "PUT",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify(updTask),
+		});
+
+		const data = await res.json();
+
 		setTasks(
 			tasks.map((task) =>
-				task.id === id ? { ...task, reminder: !task.reminder } : task
+				task.id === id ? { ...task, reminder: data.reminder } : task
 			)
 		);
 	};
